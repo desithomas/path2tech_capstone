0d19d7af05ed444fec627606fb52606d40ca24a1
Desiree  Thomas <desireethomas@mac.mynetworksettings.com>
2025-04-02 22:37:47 -0400

    This is the first commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+    <div className="header-contents">
+      <h2>Order your favorite Pixel food here</h2>
+      <p> Choose from a diverse menu featuring the very best of pixel food. It's pixelicious!</p>
+      <button>View Menu</button>
+      </div>
+      </div>
+  )
+}
+
+export default Header
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+/* This is the home page  */
+import React from 'react'
+import './Home.css'
+import Header from '../../components/Header/Header'
+
+const Home = () => {
+  return (
+    <div>
+        <Header />
+    </div>
+  )
+}
+
+export default Home
